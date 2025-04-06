68a3ece841a004e3a9eaf82decb7c2b5ff39cdcf
Kamal Adusumilli <kamaladusumilli@Kamals-MacBook-Pro.local>
2025-04-06 09:38:38 -0500

    Added prepend method; refactored code and added node class

--- linkedList.js
+++ linkedList.js
@@ -1,25 +1,37 @@
-class LinkedList {
+class Node {
   constructor(value) {
-    this.head = {
+    this.node = {
       value: value,
       next: null
-    };
+    }
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
     this.tail = this.head;
     this.length = 1;
   }
   // add value at the tail
   append(value) {
-    const newNode = {
-      value: value,
-      next: null
-    };
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  // add value at the head
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head
+    this.head = newNode
     this.length++;
     return this;
   }
 }
 
 let myLinkedList = new LinkedList(10);
-myLinkedList.append(5);
+myLinkedList.append(15);
+myLinkedList.prepend(5);
 console.log(myLinkedList);
