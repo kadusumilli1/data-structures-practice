2dbd1a4ce59cd735dab6834ca9da60213067db76
Kamal Adusumilli <kamaladusumilli@Kamals-MacBook-Pro.local>
2025-04-08 09:20:47 -0500

    Updated enqueue logic and added dequeue method

--- Queues/queueLinkedList.js
+++ Queues/queueLinkedList.js
@@ -25,22 +25,44 @@ class Queue {
             const newNode = new Node(value);
             this.first = newNode;
             this.last = newNode;
+            console.log("Enqueued: ", newNode);
         } else {
             const newNode = new Node(value);
-            this.first.next = newNode;
+            if (this.length === 1) {
+                this.first.next = newNode;
+            } else {
+                this.last.next = newNode;
+            }
             this.last = newNode;
+            console.log("Enqueued: ", newNode);
         }
         this.length++;
         return this;
     }
 
-    // dequeue() {
-
-    // }
+    dequeue() {
+        if (this.length === 0) {
+            return "Queue is empty; nothing to dequeue: " + this.first;
+        } else {
+            const newFirst = this.first.next;
+            console.log("Dequeued: ", this.first);
+            this.first = newFirst;
+            if (this.length === 1) {
+                this.last = this.first;
+            }
+        }
+        this.length--;
+        return this;
+    }
 }
 
 let myQueue = new Queue();
 console.log(myQueue.peek());
+console.log(myQueue.dequeue());
 console.log(myQueue.enqueue(5));
 console.log(myQueue.peek());
 console.log(myQueue.enqueue(10));
+console.log(myQueue.enqueue(15));
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
