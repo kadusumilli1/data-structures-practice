e41058375c62ff4705cd983137be8a76f0792a5e
Kamal Adusumilli <kamaladusumilli@Kamals-MacBook-Pro.local>
2025-04-09 15:53:55 -0500

    Added iterative implementation of BST insert method

--- Tree/binarySearchTree.js
+++ Tree/binarySearchTree.js
@@ -11,34 +11,66 @@ class BinarySearchTree {
         this.root = null;
     }
 
-    // insert node in to the tree
+    // insert node in to the tree - iterative
     insert(value) {
         const newNode = new Node(value);
         if (this.root === null) {
             this.root = newNode;
         } else {
-            this._insertNode(this.root, newNode)
+            let currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
+                    // go left
+                    if (currentNode.left === null) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    // continue loop until you reach 
+                    // a node whose left is null
+                    currentNode = currentNode.left;
+                } else {
+                    // go right
+                    if (currentNode.right === null) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    // continue loop until you reach 
+                    // a node whose right is null
+                    currentNode = currentNode.right;
+                }
+            }
         }
-        return this;
     }
 
-    _insertNode(current, newNode) {
-        if (newNode.value < current.value) {
-            if (current.left === null) {
-                current.left = newNode;
-            }
-            else {
-                this._insertNode(current.left, newNode)
-            }
-        } else {
-            if (current.right === null) {
-                current.right = newNode;
-            }
-            else {
-                this._insertNode(current.right, newNode)
-            }
-        }
-    }
+    // insert node in to the tree - recursive
+    // T.C. - O(n) and S.C. - O(n) vs O(1) in iterative
+    // insert(value) {
+    //     const newNode = new Node(value);
+    //     if (this.root === null) {
+    //         this.root = newNode;
+    //     } else {
+    //         this._insertNode(this.root, newNode)
+    //     }
+    //     return this;
+    // }
+
+    // _insertNode(current, newNode) {
+    //     if (newNode.value < current.value) {
+    //         if (current.left === null) {
+    //             current.left = newNode;
+    //         }
+    //         else {
+    //             this._insertNode(current.left, newNode)
+    //         }
+    //     } else {
+    //         if (current.right === null) {
+    //             current.right = newNode;
+    //         }
+    //         else {
+    //             this._insertNode(current.right, newNode)
+    //         }
+    //     }
+    // }
 }
 
 let tree = new BinarySearchTree();
